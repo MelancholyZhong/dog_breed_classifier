6d4595cb9ea92987a99ddfb550fba76d9756609f
MelancholyZhong <devin.zy36@gmail.com>
2023-04-12 21:58:23 -0700

    task1:digit gan done

---
+++ breed_GAN.py
@@ -0,0 +1,28 @@
+# CS5330 Project 5: Recognition using Deep Network
+# Task 1: Build a network, train it and save it
+# Author: Yao Zhong, zhong.yao@northeastern.edu
+
+# import statements
+import numpy as np 
+import pandas as pd
+
+import sys
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import torchvision.models as models
+import matplotlib.pyplot as plt
+
+
+# main function
+def main(argv):
+    
+
+    return
+
+if __name__ == "__main__":
+    main(sys.argv)
